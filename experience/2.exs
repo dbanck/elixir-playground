b388cbf5cc3eb6750d1805ddab300cc3d8e9720b
Daniel Banck <daniel@dbanck.de>
2015-07-02 19:06:47 +0200

    More elegant solution for problem 2

--- experience/2.exs
+++ experience/2.exs
@@ -2,20 +2,6 @@
 # up to the number joined with commas, e.g:
 # num_to_list(10) #=> "1,2,3,4,5,6,7,8,9,10"
 
-defmodule TestModule do
-  def generate_list(n) do
-    generate_list(n - 1, to_string(n))
-  end
-
-  def generate_list(1, result) do
-    "1," <> result
-  end
-
-  def generate_list(n, result) do
-    generate_list(n - 1, to_string(n) <> "," <> result)
-  end
-end
-
 def num_to_list(n) do
-  TestModule.generate_list(n)
+  Enum.join(1..n, ",")
 end
